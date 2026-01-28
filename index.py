7aef90e1e97611d3584df4715e4eb9a3bd079b9a
lili <lilicolombain06@gmail.com>
2026-01-28 15:21:11 +0100

    test pour import node de streetmap

--- index.py
+++ index.py
@@ -1,8 +1,19 @@
+import networkx as nx
 import osmnx as ox
 import folium
 
 
+ox.settingsuse_cache = True
+G = ox.graph.graph_from_point((47.63995, 6.86199), dist=750, network_type="drive")
+G = ox.routing.add_edge_speeds(G, hwy_speeds=None, fallback=None, agg=numpy.mean)
+G = ox.routing.add_edge_travel_times(G)
+
+ox.io.save_graph_geopackage(G, filepath="./graph.gpkg")
+
+
 orig = (47.6397, 6.8645) 
+
+chemin = nx.Graph()
 
 m = folium.Map(location=orig, zoom_start=14)
 
